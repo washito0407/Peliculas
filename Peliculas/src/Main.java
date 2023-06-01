2ddb0772dc73ee958a9dd023bb7b564a1a814828
NayeliBarrera <melani_nayhely@outlook.es>
2023-06-01 18:34:09 -0500

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	.idea/.gitignore

--- Peliculas/src/Main.java
+++ Peliculas/src/Main.java
@@ -1,8 +1,52 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-   public class Genero{
+   public class Peliculas{
        String nombre;
+       String genero;
+       int anio;
+
+       public Peliculas(String nombre, String genero, int anio, String director) {
+           this.nombre = nombre;
+           this.genero = genero;
+           this.anio = anio;
+           this.director = director;
+       }
+
+       public void setNombre(String nombre) {
+           this.nombre = nombre;
+       }
+
+       public void setGenero(String genero) {
+           this.genero = genero;
+       }
+
+       public void setAnio(int anio) {
+           this.anio = anio;
+       }
+
+       public void setDirector(String director) {
+           this.director = director;
+       }
+
+       public String getNombre() {
+           return nombre;
+       }
+
+       public String getGenero() {
+           return genero;
+       }
+
+       public int getAnio() {
+           return anio;
+       }
+
+       public String getDirector() {
+           return director;
+       }
+
+       String director;
+
     }
 
 }
