416e623baad2db0179697309cc8d7080ec68da8a
vinculando_intellij <git@github.com:SandovalBrandon1027/vinculando_intellij.git>
2023-06-01 21:18:24 -0500

    Peliculas de accion, completadas

--- Peliculas/src/Main.java
+++ Peliculas/src/Main.java
@@ -1,52 +1,88 @@
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-   public class Peliculas{
-       String nombre;
-       String genero;
-       int anio;
+    public class Peliculas {
 
-       public Peliculas(String nombre, String genero, int anio, String director) {
-           this.nombre = nombre;
-           this.genero = genero;
-           this.anio = anio;
-           this.director = director;
-       }
 
-       public void setNombre(String nombre) {
-           this.nombre = nombre;
-       }
+        String nombre;
+        String genero;
+        int anio;
 
-       public void setGenero(String genero) {
-           this.genero = genero;
-       }
+        public Peliculas(String nombre, String genero, int anio, String director) {
+            this.nombre = nombre;
+            this.genero = genero;
+            this.anio = anio;
+            this.director = director;
+        }
 
-       public void setAnio(int anio) {
-           this.anio = anio;
-       }
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
 
-       public void setDirector(String director) {
-           this.director = director;
-       }
+        public void setGenero(String genero) {
+            this.genero = genero;
+        }
 
-       public String getNombre() {
-           return nombre;
-       }
+        public void setAnio(int anio) {
+            this.anio = anio;
+        }
 
-       public String getGenero() {
-           return genero;
-       }
+        public void setDirector(String director) {
+            this.director = director;
+        }
 
-       public int getAnio() {
-           return anio;
-       }
+        public String getNombre() {
+            return nombre;
+        }
 
-       public String getDirector() {
-           return director;
-       }
+        public String getGenero() {
+            return genero;
+        }
 
-       String director;
+        public int getAnio() {
+            return anio;
+        }
+
+        public String getDirector() {
+            return director;
+        }
+
+        String director;
 
     }
 
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int opcion;
+
+        System.out.println("Menu de peliculas: ");
+        System.out.println("1) Peliculas de accion");
+        System.out.println("2) Peliculas de comedia");
+        System.out.println("3) Peliculas de terror");
+        System.out.println("4) K-doramas");
+        System.out.println("Elija una opcion:  ");
+        opcion = entrada.nextInt();
+
+        if (opcion == 1) {
+            Peliculas_accion lista = new Peliculas_accion();
+            lista.Accion();
+
+        }
+
+        if(opcion == 2){
+            System.out.println("Peliculas de accion");
+        }
+        if(opcion == 3){
+            System.out.println("Peliculas de terror");
+        }
+        if(opcion ==4){
+            System.out.println("k-doramas");
+        }
+
+
+
+
+    }
 }
--- Peliculas/src/Peliculaskdramas.java
+++ Peliculas/src/Peliculaskdramas.java
@@ -1,10 +1,9 @@
 public class Peliculaskdramas {
 
     public static void main(String[] args){
-        Peliculas blind=new Peliculas;
-        Peliculas sanador=new Peliculas;
-        Peliculas Sowdrop=new Peliculas;
-        Peliculas eve=new Peliculas;
+
+
+
 
     }
 }
