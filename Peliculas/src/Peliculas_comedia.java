edfb0d96a1fb19ec10c384b5652f64db8abd583c
washito0407 <washovilla78@gmail.com>
2023-06-02 01:01:27 -0800

    Merge remote-tracking branch 'origin/main'

--- Peliculas/src/Peliculas_comedia.java
+++ Peliculas/src/Peliculas_comedia.java
@@ -7,18 +7,5 @@ public class Peliculas_comedia {
         System.out.println("Gato con botas");
         System.out.println("Comando especial");
 
-        Scanner entrada = new Scanner(System.in);
-        int opcion;
-
-        if (opcion == 2) {
-            Metodo accion = new Metodo("Zoolander", " comedia", 2001, " Ben Stiller");
-            accion.mostrar();
-            Metodo accion1 = new Metodo("¿Quien carajo son los Miller", " comedia", 2013, " Rawson Marshall Thurber");
-            accion1.mostrar();
-            Metodo accion2 = new Metodo("Gato con botas", " comedia", 1969, " Kimio Yabuki");
-            accion2.mostrar();
-            Metodo accion3 = new Metodo("Comando especial", " comedia", 2012, " Phil Lord y Christopher Miller");
-            accion3.mostrar();
-        }
     }
 }
