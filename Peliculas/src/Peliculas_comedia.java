e939385511373270e38f50f5c61ac668ebf315c3
washito0407 <washovilla78@gmail.com>
2023-06-02 00:55:03 -0800

    Merge remote-tracking branch 'origin/main'

--- Peliculas/src/Peliculas_comedia.java
+++ Peliculas/src/Peliculas_comedia.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Peliculas_comedia {
     public void comedia(){
         System.out.println("Las peliculas de comedia son: ");
@@ -6,6 +7,17 @@ public class Peliculas_comedia {
         System.out.println("Gato con botas");
         System.out.println("Comando especial");
 
+        if (opcion == 1) {
+            Metodo accion = new Metodo("Zoolander", "comedia",2001, "Ben Stiller");
+            accion.mostrar();
+            Metodo accion1 = new Metodo("¿Quien carajo son los Miller", "comedia",2007," Zack Snyder");
+            accion1.mostrar();
+            Metodo accion2 = new Metodo("Vikingos","comedia", 2013, "Michael Hirst");
+            accion2.mostrar();
+            Metodo accion3 = new Metodo("Gato con botas","comedia",2016,"Robert Zemeckis");
+            accion3.mostrar();
+
+        }
 
     }
 
